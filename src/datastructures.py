c567a8723eb811312858a834845d08d4d5c31a92
aaguirre <alejandra36a@gmail.com>
2024-09-09 11:10:37 +0000

    primer cambio

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,29 +8,55 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
+        self._next_id = 1
+        self._members = [
+            {
+                "name": "John",
+                "age": 33,
+                "Lucky_Numbers": [7,13,22],
+                "id": self._generate_id(),
+                "last_name": self.last_name
+            },
+            {
+                "name": "Jane",
+                "age": 35,
+                "Lucky_Numbers": [10, 14, 3],
+                "id": self._generate_id(),
+                "last_name": self.last_name
+            },
+            {
+                "name": "Jimmy",
+                "age": 5,
+                "Lucky_Numbers": [1],
+                "id": self._generate_id(),
+                "last_name": self.last_name
+            }
+        ]
 
-        # example list of members
-        self._members = []
+    # Este método genera un 'id' único al agregar miembros a la lista (no debes modificar esta función)
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
-
+    
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        ## Debes implementar este método
+        ## Recorre la lista y elimina el miembro con el id proporcionado
+        
 
     def get_member(self, id):
-        # fill this method and update the return
+        ## Debes implementar este método
+        ## Recorre la lista y obtén el miembro con el id proporcionado
         pass
 
-    # this method is done, it returns a list with all the family members
-    def get_all_members(self):
+    def get_all_members(self, id):
         return self._members
